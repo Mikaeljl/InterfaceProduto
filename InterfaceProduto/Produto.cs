2133d6a3052cb99448ea32337cdc03861181f558
Mikael de Jesus <mikaeljl824@gmail.com>
2024-11-19 20:29:25 -0300

    Adicionar arquivos de projeto.

---
+++ InterfaceProduto/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceProduto
+{
+    public class PagamentoCartao : IProduto
+
+    {
+
+        public void RealizarPagamento()
+
+        {
+
+            Console.WriteLine("Pagamento realizado via cartão.");
+
+        }
+    }
+}
---
+++ InterfaceProduto/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceProduto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IProduto pagamento = new PagamentoCartao();
+
+            pagamento.RealizarPagamento();  // Chama o método implementado pela classe concreta
+        }
+    }
+}
